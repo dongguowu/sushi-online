30dba65b6f0c9279b882f2818300e71b170d7eeb
Dongguo WU <david.dong.guo@gmail.com>
2023-03-31 15:11:37 -0400

    move css and js import to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import 'js/bootstrap.bundle.min.js'
+import 'js/holder.min.js'
+
+import 'css/index.css'
+
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
